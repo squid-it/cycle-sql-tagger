efdaf42cca6dfaff792c10b6176e2b4e2c84063b
Cecil Zorg <moroswitie@users.noreply.github.com>
2026-05-24 12:46:31 +0200

    Add logger (#8)
    * added DbLogger
    
    * Update github actions
    
    * Added fixed size logger, added DbLoggerInterface

--- tests/Unit/DatabaseManagerWithTaggerTest.php
+++ tests/Unit/DatabaseManagerWithTaggerTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace SquidIT\Tests\Cycle\Sql\Tagger\Unit;
 
 use Cycle\Database\LoggerFactoryInterface;
-
 use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
